0809db20e0c835cbc4994674f7c449f9dc46d764
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2023-09-09 20:35:37 +0600

    fixing the issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,26 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+document.addEventListener('DOMContentLoaded', () => {
+   const navMenu = document.getElementById('nav_menu');
+   const navToggle = document.getElementById('nav-toggle');
+   const navClose = document.getElementById('nav-close');
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-}
+   /*===== MENU SHOW =====*/
+   /* Validate if constant exists */
+   if (navToggle) {
+      navToggle.addEventListener('click', () => {
+         navMenu.classList.add('show-menu');
+      });
+   }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
-}
+   /*===== MENU HIDDEN =====*/
+   /* Validate if constant exists */
+   if (navClose) {
+      navClose.addEventListener('click', () => {
+         navMenu.classList.remove('show-menu');
+      });
+   }
+});
+
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
